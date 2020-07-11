d7d78563893ad06743c263341882b44b082b97d1
Andrew Ghostuhin <me@torinasakura.name>
2020-07-11 22:08:16 +0300

    feat: init
    Signed-off-by: Andrew Ghostuhin <me@torinasakura.name>

---
+++ packages/prettier-config/src/index.ts
@@ -0,0 +1,11 @@
+const config = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  pluginSearchDirs: [process.cwd()],
+  printWidth: 100,
+};
+
+export default config
