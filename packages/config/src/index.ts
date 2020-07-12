45ac45015059dbd7f70b29e4b324fc7a06b315c7
Andrew Ghostuhin <me@torinasakura.name>
2020-07-12 13:52:51 +0300

    feat: init
    Signed-off-by: Andrew Ghostuhin <me@torinasakura.name>

---
+++ packages/config/src/index.ts
@@ -0,0 +1,4 @@
+export { JEST_CONFIG_PATH } from '@atlantis-lab/jest-config/dist/paths'
+export { ESLINT_CONFIG_PATH, ESLINT_IGNORE_PATH } from '@atlantis-lab/eslint-config/dist/paths'
+export { PRETTIER_CONFIG_PATH, PRETTIER_IGNORE_PATH } from '@atlantis-lab/prettier-config/dist/paths'
+export { COMMITLINT_CONFIG_PATH } from '@atlantis-lab/commitlint-config/dist/paths'
